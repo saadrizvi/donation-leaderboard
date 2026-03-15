86c6e7a9be808d6a964a2f92ed0ad77a65d22227
Saad Rizvi <saad.rizvi@outlook.com>
2026-03-15 14:58:19 -0400

    base

---
+++ client/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Donor Board</h1>;
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const apiRouter = require('./routes/api');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', apiRouter);
+
+const distPath = path.join(__dirname, '../client/dist');
+app.use(express.static(distPath));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
